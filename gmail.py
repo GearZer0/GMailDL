b0835b7767b5c36181ad24a83f503ad94a9fd71e
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2020-09-07 16:47:24 +0800

    initial upload

---
+++ gmail.py
@@ -0,0 +1,80 @@
+#source : https://stackoverflow.com/questions/61366836/download-attachment-from-mail-using-python/61368192#61368192
+
+import os
+from imbox import Imbox # pip install imbox
+import traceback
+from datetime import date
+
+# enable less secure apps on your google account
+# https://myaccount.google.com/lesssecureapps
+
+host = "imap.gmail.com"
+username = " " #input account
+password = ' ' #input password
+download_folder = " " #input file path
+
+if not os.path.isdir(download_folder):
+    os.makedirs(download_folder, exist_ok=True)
+    
+mail = Imbox(host, username=username, password=password, ssl=True, ssl_context=None, starttls=False)
+messages = mail.messages(unread=True) # Unread messages
+
+for (uid, message) in messages:
+    mail.mark_seen(uid) # optional, mark message as read
+
+    for idx, attachment in enumerate(message.attachments):
+        try:
+            att_fn = attachment.get('filename')
+            download_path = f"{download_folder}/{att_fn}"
+            print(download_path)
+            with open(download_path, "wb") as fp:
+                fp.write(attachment.get('content').read())
+        except:
+            pass
+            print(traceback.print_exc())
+
+mail.logout()
+
+
+"""
+Available Message filters: 
+
+# Gets all messages from the inbox
+messages = mail.messages()
+
+# Unread messages
+messages = mail.messages(unread=True)
+
+# Flagged messages
+messages = mail.messages(flagged=True)
+
+# Un-flagged messages
+messages = mail.messages(unflagged=True)
+
+# Flagged messages
+messages = mail.messages(flagged=True)
+
+# Un-flagged messages
+messages = mail.messages(unflagged=True)
+
+# Messages sent FROM
+messages = mail.messages(sent_from='sender@example.org')
+
+# Messages sent TO
+messages = mail.messages(sent_to='receiver@example.org')
+
+# Messages received before specific date
+messages = mail.messages(date__lt=datetime.date(2018, 7, 31))
+
+# Messages received after specific date
+messages = mail.messages(date__gt=datetime.date(2018, 7, 30))
+
+# Messages received on a specific date
+messages = mail.messages(date__on=datetime.date(2018, 7, 30))
+
+# Messages whose subjects contain a string
+messages = mail.messages(subject='Christmas')
+
+# Messages from a specific folder
+messages = mail.messages(folder='Social')
+"""
